715af45c334bc83fe46af5671850413c6ab5be61
Mondomod <mondomod@yahoo.com>
2026-07-22 17:15:42 -0400

    Update dimension_iv.mk

--- dimension_iv.mk
+++ dimension_iv.mk
@@ -1,20 +1,34 @@
 # --------------------------------------------------------------
-# LV2 Packaging
-# Copies custom LV2 metadata and MOD GUI resources into
-# the generated LV2 bundle.
+#
+# Dimension_IV.lv2
+#
 # --------------------------------------------------------------
 
+DIMENSION_IV_VERSION = 359e5e9c7a2e5899ccce0398263d9da4b882dc8e
 DIMENSION_IV_SITE = https://github.com/Mondomod/Dimension_IV.git
 DIMENSION_IV_SITE_METHOD = git
-DIMENSION_IV_GIT_SUBMODULES = y
+DIMENSION_IV_GIT_SUBMODULES = YES
+
 DIMENSION_IV_BUNDLES = Dimension_IV.lv2
 
-
-# needed for submodules support
-DIMENSION_IV_PRE_DOWNLOAD_HOOKS += MOD_PLUGIN_BUILDER_DOWNLOAD_WITH_SUBMODULES
-
-define DIMENSION_IV_INSTALL_TARGET_CMDS
-	cp -rL $(@D)/Dimension_IV.lv2 $(TARGET_DIR)/usr/lib/lv2/
+define DIMENSION_IV_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/plugin/source lv2_sep
 endef
 
-$(eval $(cmake-package))
+define DIMENSION_IV_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/lv2/Dimension_IV.lv2
+
+	cp -rL $(@D)/bin/Dimension_IV.lv2/* \
+		$(TARGET_DIR)/usr/lib/lv2/Dimension_IV.lv2/
+
+	cp -f $(@D)/lv2/*.ttl \
+		$(TARGET_DIR)/usr/lib/lv2/Dimension_IV.lv2/
+
+	cp -rf $(@D)/lv2/modgui \
+		$(TARGET_DIR)/usr/lib/lv2/Dimension_IV.lv2/ 2>/dev/null || true
+
+	cp -rf $(@D)/lv2/images \
+		$(TARGET_DIR)/usr/lib/lv2/Dimension_IV.lv2/ 2>/dev/null || true
+endef
+
+$(eval $(generic-package))
